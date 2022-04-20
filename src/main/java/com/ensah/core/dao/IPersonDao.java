77fd99603413d3c6367fd348d405fa3470e58adb
Ennoukra Abdelghani <ghaniennoukra@gmail.com>
2022-04-20 21:37:33 +0000

    add all

---
+++ src/main/java/com/ensah/core/dao/IPersonDao.java
@@ -0,0 +1,14 @@
+package com.ensah.core.dao;
+
+import com.ensah.core.bo.Person;
+import com.genericdao.api.GenericDao;
+
+/**
+ * Interface du DAO
+ * 
+ * @author T. BOUDAA
+ *
+ */
+public interface IPersonDao extends GenericDao<Person, Long> {
+
+}
